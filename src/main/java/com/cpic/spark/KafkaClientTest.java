8cb03c6854a0865248499a67b68f1fdcc0e5c478
AdolphCN <676945813@qq.com>
2020-04-22 19:33:19 +0800

    rm error

--- src/main/java/com/cpic/spark/KafkaClientTest.java
+++ src/main/java/com/cpic/spark/KafkaClientTest.java
@@ -2,7 +2,7 @@ package com.cpic.spark;
 
 public class KafkaClientTest {
     public static void main(String[] args) {
-        new KafkaConsumerTest(KafkaProperties.TOPIC).start();
-        new KafkaProducerTest(KafkaProperties.TOPIC).start();
+//        new KafkaConsumerTest(KafkaProperties.TOPIC).start();
+//        new KafkaProducerTest(KafkaProperties.TOPIC).start();
     }
 }
